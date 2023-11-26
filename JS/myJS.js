d11e917f68a616b0d09b2b1e65e1677c7734eef3
sarfrajabbasi <sarfrajk96811@gmail.com>
2023-11-26 10:51:42 +0530

    lead-tracker

---
+++ JS/myJS.js
@@ -0,0 +1,14 @@
+// data-is any thing that meaning full to computer.
+
+// data-type in js = undefined null boolean,string,number bigInt,symbol,object.
+
+// store data in variable
+
+// variable is label that allow data to store and manupulate.
+
+// var & let is changable and but const is not. 
+
+var a = "abc";
+  a = 134;
+let b = "aa";
+const c = 1;
