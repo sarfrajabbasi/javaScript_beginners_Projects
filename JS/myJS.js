a5a9e7f714767788e40354f61d053f302c88b7a5
sarfrajabbasi <sarfrajk96811@gmail.com>
2023-11-26 11:16:31 +0530

    javscript concept

--- JS/myJS.js
+++ JS/myJS.js
@@ -6,13 +6,56 @@
 
 // variable is label that allow data to store and manupulate.
 
-// var & let is changable and but const is not. 
+// var & let is changable and but const is not.
 
 var a = "abc";
 //   var a(declare) & = "abc"(initialized it)
-  a = 134;
+a = 134;
 let b = "aa";
 const c = 1;
 
 // var and functions are caSe sensitive.
 
+var myNum = 56;
+myNum++;
+myNum--;
+var decilmalNum = 5.7;
+decilmalNum = 0.009;
+
+// Reminder
+var rem = 11 % 3;
+
+// Agumented the variable
+
+var allMe = 12;
+var thenCall = 34;
+var callMe = 67;
+
+allMe += 1;
+thenCall += 1;
+callMe += 1;
+
+allMe -= 1;
+thenCall -= 1;
+callMe -= 1;
+
+allMe *= 1;
+thenCall *= 1;
+callMe *= 1;
+
+allMe /= 1;
+thenCall /= 1;
+callMe /= 1;
+
+console.log(allMe + thenCall + callMe);
+
+// Declare String var
+
+let firstName = "Sarfraj";
+let lastName = " Abbasi";
+
+// Escaping Literal Quotes in Strings
+let mylife = "Succk all \"thing mess up an no hope in my \"life becoz \" those mental\" peace"
+
+console.log(firstName + lastName);
+console.log(mylife);
